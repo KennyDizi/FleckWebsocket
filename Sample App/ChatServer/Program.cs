fc6bfc15cfc29044ceb03c5fc4271165c7f87517
jonlt <jonlt@localhost>
2010-06-01 02:34:14 +0200

    initial commit

---
+++ Sample App/ChatServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocketServer;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample web socket chat app:");
+            Console.WriteLine("Navigate to http://localhost:8080/ to start chatting...\n");
+            var cs = new ChatServer();
+        }
+    }
+}
