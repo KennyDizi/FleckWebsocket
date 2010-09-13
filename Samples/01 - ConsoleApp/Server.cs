bf71a19f67422b4539f4dd99bdc4042294bc514d
jonlt <jonlt@localhost>
2010-09-13 14:11:17 +0200

    added some comments

--- Samples/01 - ConsoleApp/Server.cs
+++ Samples/01 - ConsoleApp/Server.cs
@@ -6,21 +6,28 @@ using Nugget;
 
 namespace ConsoleApp
 {
-    class CLISocket : WebSocket
-    {
-        public static List<CLISocket> Connections = new List<CLISocket>();
 
+    // The server side socket
+    class ConsoleAppSocket : WebSocket
+    {
+        // a list of connected sockets
+        public static List<ConsoleAppSocket> Connections = new List<ConsoleAppSocket>();
+
+        // This method is called when data is comming from the client.
+        // In this example the method is just empty
         public override void Incomming(string data)
         {
-            Console.WriteLine(data);
+
         }
 
+        // This method is called when the socket disconnects
         public override void Disconnected()
         {
             Console.WriteLine("--- disconnected ---");
             Connections.Remove(this);
         }
 
+        // This method is called when the socket connects
         public override void Connected(ClientHandshake handshake)
         {
             Console.WriteLine("--- connected --- ");
@@ -28,23 +35,34 @@ namespace ConsoleApp
         }
     }
     
+
     class Server
     {
         static void Main(string[] args)
         {
+            // create the server
+            // the parameters describe where to listen for connections (the port) and which connections to accept (the origin and location)
+            // it is important that these are correct, or the server might not accept the incomming connections
+            // see http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol, to learn more about these parameters
             var nugget = new WebSocketServer(8181, "null", "ws://localhost:8181");
-            nugget.RegisterHandler<CLISocket>("/clisample");
+
+            // register the ConsoleAppSocket class to handle connection comming to /consoleappsample
+            nugget.RegisterHandler<ConsoleAppSocket>("/consoleappsample");
+            // no logging
             Nugget.Log.Level = LogLevel.None;
+            // start the server
             nugget.Start();
             
+            // info
             Console.WriteLine("Server started, open client.html in a websocket-enabled browser\n");
             Console.WriteLine("Once the connection is established\nanything you type into this console, will be send to the browser");
 
 
+            // keep alive loop
             var input = Console.ReadLine();
             while (input != "exit")
             {
-                foreach (var item in CLISocket.Connections)
+                foreach (var item in ConsoleAppSocket.Connections)
                     item.Send(input);
                 
                 input = Console.ReadLine();
--- Samples/02 - SubProtocol/Server.cs
+++ Samples/02 - SubProtocol/Server.cs
@@ -7,6 +7,7 @@ using JONSParser;
 
 namespace SubProtocol
 {
+    // a "model" representing a post made by someone
     class Post
     {
         public string Author { get; set; }
@@ -19,18 +20,29 @@ namespace SubProtocol
         }
     }
 
+    // the model factory
+    // this class is used to convert the data comming from the client into an model object
     class PostFactory : ISubProtocolModelFactory<Post>
     {
+        // create a new model
+        // "data" is the data comming from the client
+        // "connection" is an object representing the connection to the client, it can be used to identify the client if needed
         public Post Create(string data, WebSocketConnection connection)
         {
+            // parse the data (we assume that the data string is on json format)
             var js = JSON.Parse(data);
             if (js.hasOwnProperty("author") && js.hasOwnProperty("body"))
             {
+                // create a new model using the data from the client
+                // "return" passes the model on to the handler class
                 return new Post() { Author = js.author, Body = js.body };
             }
             return null;
         }
 
+        // this method is used to define valid models
+        // if a model is not valid, then it is not passed on to the handler class
+        // this method is called internally in the server before the model is passed on
         public bool IsValid(Post p)
         {
             
@@ -41,8 +53,13 @@ namespace SubProtocol
         }
     }
 
+    // the handler class
+    // note that the class inherits the generic interface and uses the model as the type parameter
     class PostSocket : WebSocket<Post>
     {
+        // this method is called when data is comming from the client
+        // note that the method takes a Post object instead of a string
+        // this is the object created in the model factory
         public override void Incomming(Post post)
         {
             Console.WriteLine("{0} posted {1}",post.Author,post.Body);
@@ -63,12 +80,22 @@ namespace SubProtocol
     {
         static void Main(string[] args)
         {
+            // create the server
             var nugget = new WebSocketServer(8181, "null", "ws://localhost:8181");
+            // register the handler
             nugget.RegisterHandler<PostSocket>("/subsample");
+            // register the model factory
+            // this is important since we need the model factory to create the models
+            // the string passed is the name of the sub protocol that the factory should be used for
+            // any client connecting with this subprotocol, will have all its data send throught the factory
             nugget.SetSubProtocolModelFactory(new PostFactory(), "post");
+            
+            // start the server
             nugget.Start();
+            // info
             Console.WriteLine("Server started, open client.html in a websocket-enabled browser");
             
+            // keep alive
             var input = Console.ReadLine();
             while (input != "exit")
             {
