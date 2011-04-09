a65e8824d8611bcc12db01cd70746f79c6fde3fa
jonlt <jonlt@localhost>
2011-04-09 17:12:57 +0200

    chat sample

--- Samples/02 - Chat/Server.cs
+++ Samples/02 - Chat/Server.cs
@@ -12,12 +12,23 @@ namespace Chat
         {
             // create the server
             var nugget = new WebSocketServer("ws://localhost:8181", "null");
-
-            // subscribe to the OnConnect event
+            
             nugget.OnConnect += (wsc) =>
             {
-                wsc.Send("Hello World");
+                wsc.Send("[server] Welcome to the chat");
+                nugget.SendToAll(String.Format("[server] {0} connected", wsc.Socket.RemoteEndPoint), wsc);
                 Console.WriteLine("new connection from {0}", wsc.Socket.RemoteEndPoint);
+                
+                wsc.OnReceive += (sender, data) =>
+                {
+                    nugget.SendToAll(String.Format("[{0}] {1}",sender.Socket.RemoteEndPoint,data));
+                };
+
+                wsc.OnDisconnect += (connection) =>
+                {
+                    nugget.SendToAll(String.Format("[server] {0} disconnected", connection.Socket.RemoteEndPoint), connection);
+                };
+
             };
 
             // start the server
@@ -28,8 +39,9 @@ namespace Chat
             while (input != "exit")
             {
                 input = Console.ReadLine();
-                nugget.SendToAll(input);
+                nugget.SendToAll("[server] "+input);
             }
         }
+
     }
 }
