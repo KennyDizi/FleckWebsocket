8582bb55a77786e2711aa2f8914022351f3b8f54
jon <jon@jon-PC>
2012-10-07 13:21:20 +0200

    updating according to protocol changes

---
+++ src/Nugget.Server/EventArguments/DataReceivedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nugget.Server
+{
+    public class DataReceivedEventArgs : EventArgs
+    {
+        public IEnumerable<DataFragment> Fragments { get; set; }
+
+        public DataReceivedEventArgs(IEnumerable<DataFragment> fragments)
+        {
+            Fragments = fragments;
+        }
+
+    }
+}
